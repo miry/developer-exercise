df96fb603219dbf267584fbf88cc19709d7d2ffa
Michael Nikitochkin <miry.sof@gmail.com>
2012-06-07 11:33:06 +0300

    #1: Implemented Array extension.

--- ruby/where/test.rb
+++ ruby/where/test.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require File.expand_path('./where.rb', File.dirname(__FILE__))
 
 class WhereTest < Test::Unit::TestCase
   def setup
@@ -11,7 +12,7 @@ class WhereTest < Test::Unit::TestCase
   end
   
   def test_where_with_exact_match
-    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf'),
+    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf')
   end
   
   def test_where_with_partial_match
---
+++ ruby/where/where.rb
@@ -0,0 +1,13 @@
+module WhereExtension
+  def where(conditions={})
+    result = self.dup
+    conditions.each do |key, val|
+      result.reject!{|item| (!item.has_key?(key) || !(val.is_a?(Regexp) ? item[key] =~ val : item[key] == val) )  }
+    end
+    result
+  end
+end
+
+class Array
+  include WhereExtension
+end
